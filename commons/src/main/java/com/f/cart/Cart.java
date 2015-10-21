bdcb796f9fa02932c43201e71238f10eb2b18fc7
fengmingming <1053931671@qq.com>
2015-10-21 21:15:08 +0800

    cart

---
+++ commons/src/main/java/com/f/cart/Cart.java
@@ -0,0 +1,91 @@
+package com.f.cart;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import framework.exception.BusinessException;
+
+
+//字符串表示 cgid#cgid#cgId_number_type1#type2@ 
+public class Cart implements Serializable{
+
+	private static final long serialVersionUID = -1599236470983369139L;
+	public static final String SEPARATOR_1 = "_";
+	public static final String SEPARATOR_2 = "#";
+	
+	private List<Long> cgidList = new ArrayList<Long>(3);
+	private int number = 1;
+	
+	public Cart(String cart){
+		if(cart == null||cart.length() == 0){
+			throw new BusinessException(112L);
+		}
+		String[] arr = cart.split(SEPARATOR_1);
+		for(String cgid:arr[0].split(SEPARATOR_2)){
+			this.cgidList.add(Long.parseLong(cgid));
+		}
+		this.number = Integer.parseInt(arr[1]);
+	}
+	
+	public Cart(int number, Long ... cgids){
+		this.number = number;
+		for(Long l:cgids){
+			if(l != null){
+				this.cgidList.add(l);
+			}
+		}
+	}
+	
+	public void plus(){
+		this.number++;
+	}
+	
+	public void minus(){
+		this.number--;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public List<Long> getCgidList() {
+		return cgidList;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder(toCartString());
+		sb.append(SEPARATOR_1);
+		sb.append(this.number);
+		return sb.toString();
+	}
+	
+	public String toCartString(){
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0,j=this.cgidList.size();i<j;i++){
+			sb.append(this.cgidList.get(i));
+			if(i<j-1){
+				sb.append(SEPARATOR_2);
+			}
+		}
+		return sb.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return toCartString().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Cart){
+			Cart cart = (Cart) obj;
+			return toCartString().equals(cart.toCartString());
+		}
+		return false;
+	}
+	
+}
--- commons/src/main/java/com/f/cart/Carts.java
+++ commons/src/main/java/com/f/cart/Carts.java
@@ -1,7 +1,60 @@
 package com.f.cart;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import framework.exception.BusinessException;
+
+//分隔符@
 public class Carts implements Serializable{
 
+	private static final long serialVersionUID = -31074761305932629L;
+	public static final String SEPARATOR_1 = "@";
+	
+	private List<Cart> cartList = new ArrayList<Cart>();
+	
+	public Carts(String carts){
+		if(carts == null||carts.length() == 0){
+			throw new BusinessException(112L);
+		}
+		String[] arr = carts.split(SEPARATOR_1);
+		for(String cart:arr){
+			cartList.add(new Cart(cart));
+		}
+	}
+	
+	public List<Cart> getCartList() {
+		return cartList;
+	}
+	
+	public int getCartsSize(){
+		int number = 0;
+		for(Cart cart:this.cartList){
+			number = number + cart.getNumber();
+		}
+		return number;
+	}
+	
+	public Cart getEqualsCart(Cart cart){
+		for(Cart c:this.cartList){
+			if(c.equals(cart)){
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		for(int i=0,j=this.cartList.size();i<j;i++){
+			sb.append(this.cartList.get(i).toString());
+			if(i<j-1){
+				sb.append(SEPARATOR_1);
+			}
+		}
+		return sb.toString();
+	}
+	
 }
